4fd2e5ad39b342785b76b5710b78a8a7ee529d33
dtsdesign <jfolds@dtsgis.com>
2019-10-24 16:43:57 -0600

    2 files modified, 2 added

--- src/shared/shared.js
+++ src/shared/shared.js
@@ -2,8 +2,6 @@
 // import 'bootstrap-sass/assets/javascripts/bootstrap';
 import $ from 'jquery';
 // TODO: include this only where necessary?
-// import 'jquery-validation';
-// import 'jquery-validation-unobtrusive';
 
 import './site.scss';
 
